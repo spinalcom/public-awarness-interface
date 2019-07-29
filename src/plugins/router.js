cdcd166a2258abf4dd129b1bc82b07d2fc780b22
LorisC <loris@ciprin.fr>
2019-07-29 11:34:29 +0200

    move router to plugin folder

--- src/plugins/router.js
+++ src/plugins/router.js
@@ -1,12 +1,12 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from './views/Home.vue';
+import Home from '../views/Home.vue';
 
 Vue.use(Router);
-
+// TODO check how to set env viriable like process.env.BASE_URL
 export default new Router({
   mode: 'history',
-  base: process.env.BASE_URL,
+  base: 'html/pai',
   routes: [
     {
       path: '/',
@@ -19,7 +19,7 @@ export default new Router({
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
+      component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
     },
   ],
 });
