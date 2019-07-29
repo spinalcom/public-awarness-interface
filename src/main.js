6b5ab9227caced391d53d04333e4f84290c831b7
LorisC <loris@ciprin.fr>
2019-07-29 11:38:37 +0200

    browser bootstraped

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import App from './App.vue';
-import router from './router';
+import router from './plugins/router';
 import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false;
@@ -8,5 +8,5 @@ Vue.config.productionTip = false;
 new Vue({
   router,
   vuetify,
-  render: h => h(App)
+  render: h => h(App),
 }).$mount('#app');
