08640f12c1eff1759aa2b47280cd4e266b404dfe
LorisC <loris@ciprin.fr>
2019-08-29 11:49:20 +0200

    add usermanager to vue instance prototype

--- src/main.js
+++ src/main.js
@@ -3,8 +3,12 @@ import App from './App.vue';
 import router from './plugins/router';
 import vuetify from './plugins/vuetify';
 import './plugins/cookie';
-
+import UserManager from './UserManager';
 Vue.config.productionTip = false;
+
+Vue.prototype.$userManager = new UserManager();
+
+
 
 new Vue({
   router,
