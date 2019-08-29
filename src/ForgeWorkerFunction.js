96ea269608ff1cb1dbe31299899144ac0ede59c2
LorisC <loris@ciprin.fr>
2019-08-29 13:32:17 +0200

    update register

--- src/ForgeWorkerFunction.js
+++ src/ForgeWorkerFunction.js
@@ -61,3 +61,65 @@ function userFunction( pdb ) {
   
   return m.getPropertyDb().executeUserFunction( fun );
 }
+
+
+
+/**
+ * Retrieve the value of attributeName for each dbIds
+ * @param m {Model} Forge model
+ * @param attributeName {String} name of the attribute to get the value of
+ * @return {*}
+ */
+export  function getValues( m, attributeName) {
+  const fun = `
+
+function userFunction( pdb ) {
+  const res = {};
+  let attrNameId = -1;
+  
+  function convertDate( date ) {
+    let d = "";
+    if (date.length === 8) {
+      d = date.substring( 0, 4 );
+      d += "-";
+      d += date.substring( 4, 6 );
+      d += "-";
+      d += date.substring( 6, 8 );
+    }
+    return d;
+  }
+  
+  pdb.enumAttributes( ( i, attrDef, attrRaw ) => {
+    const { name } = attrDef;
+    
+    if (name.toLowerCase() === '${attributeName.toLowerCase()}') {
+      attrNameId = i;
+      return true;
+    }
+  } );
+  
+  pdb.enumObjects( ( dbId ) => {
+    pdb.enumObjectProperties( dbId, ( attrId, valId ) => {
+      if (attrId === attrNameId) {
+        const value = pdb.getAttrValue( attrId, valId );
+        if (value.length > 0) {
+          let date = new Date(convertDate( value )).getTime();
+          if(isNaN(date)){
+          console.log('datete',value)
+          }
+          if (!res.hasOwnProperty( date ))
+            res[date] = [];
+          res[date].push(dbId)
+        }
+        return true;
+        
+      }
+    } );
+  } );
+  
+  return { dbIds: res, attrId: attrNameId };
+}`;
+  
+  return m.getPropertyDb().executeUserFunction( fun );
+}
+
