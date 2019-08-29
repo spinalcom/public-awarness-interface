37a5a4fd4858ec6f83c256d50c1c203ba04ce6c7
LorisC <loris@ciprin.fr>
2019-08-29 13:31:56 +0200

    update css

--- src/App.vue
+++ src/App.vue
@@ -27,13 +27,20 @@
       acceptCookies(){
         window.$cookies.set('cookiesAccepted', "ok", null, '/');
         this.cookieAccepted = true;
-
       }
     },
     mounted(){
       if (window.$cookies.get('cookiesAccepted'))
         this.cookieAccepted = true
-
     }
   }
 </script>
+
+<style>
+  #app{
+    overflow: hidden
+  }
+  html{
+    overflow: hidden
+  }
+</style>
