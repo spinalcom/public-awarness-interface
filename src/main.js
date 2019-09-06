b74397ef1c48b06b970fdafc1c3a7e4521793a95
LorisC <loris@ciprin.fr>
2019-09-06 14:42:28 +0200

    add vue store

--- src/main.js
+++ src/main.js
@@ -4,14 +4,15 @@ import router from './plugins/router';
 import vuetify from './plugins/vuetify';
 import './plugins/cookie';
 import UserManager from './UserManager';
+import store from "./store"
 Vue.config.productionTip = false;
 
 Vue.prototype.$userManager = new UserManager();
 
 
-
 new Vue({
   router,
   vuetify,
+  store,
   render: h => h(App),
 }).$mount('#app');
