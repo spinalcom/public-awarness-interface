4bf229b366aff8b25b2d32bec29b4ac77c2800e1
LorisC <loris@ciprin.fr>
2019-10-01 11:19:31 +0200

    adjust logo size

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,11 +5,18 @@
             <spinal-forge-viewer-vue :onInitialize="onInitialize">
 
             </spinal-forge-viewer-vue>
-            <img id="logo-colas" src="../assets/logo-colas.png"/>
-            <img id="logo-spinalcom" src="../assets/logo-spinalcom.png"/>
-            <encart-tuto v-if="initialized"> </encart-tuto>
+            <img id="logo-colas"
+                 src="../assets/logo-colas.png"
+                 :style="{width: '160px', height: '60px'}"
+                 v-if="displayLogo"
+            />
+            <img id="logo-spinalcom"
+                 src="../assets/logo-spinalcom.png"
+                 :style="{width: '140px', height: '40px'}"
+                 v-if="displayLogo">
+            <encart-tuto v-if="initialized"></encart-tuto>
         </div>
-        <div class="header" >
+        <div class="header">
 
             <timeline-video-slider
                     v-if="initialized"
@@ -58,7 +65,13 @@
         endTime: 0,
         dbIds: {},
         initialized: false,
-        lastDbIds: []
+        lastDbIds: [],
+        logoWidth: 10,
+        logoHeight: 10,
+        oldWidth: 1,
+        oldHeight: 1,
+        ratio: 10,
+        displayLogo: false
       };
     },
     computed: {
@@ -84,6 +97,7 @@
     methods: {
       onInitialize( viewerManager ) {
         this.viewerManager = viewerManager;
+        console.log( this.viewerManager )
 
         getGraph()
           .then( graph => SpinalGraphService.setGraph( graph ).then( () => {
@@ -109,13 +123,18 @@
           .then( () => this.getSvf() )
           .then( ( svf ) => {
             this.path = window.location.origin + svf;
+
             return this.viewerManager.start( this.path, true )
-          } )
-          .then( ( m ) => {
-            this.model = m;
-            this.loaded = true;
-            return getValues( this.model, this.attrName )
-          } )
+          } ).then( () => {
+          this.viewerManager.viewer.addEventListener(
+            Autodesk.Viewing.GEOMETRY_LOADED_EVENT, this.onGeometryLoadedBinded )
+        } )
+
+      },
+      onGeometryLoaded( event ) {
+        this.model = event.model;
+        this.loaded = true;
+        return getValues( this.model, this.attrName )
           .then( res => {
             this.dbIds = res['dbIds'];
             for (let key in this.dbIds) {
@@ -127,17 +146,23 @@
                 }
               }
             }
-            this.viewerManager.viewer.setReverseZoomDirection(true);
+            this.viewerManager.viewer.setReverseZoomDirection( true );
             this.viewerManager.viewer.setProgressiveRendering( false );
             this.viewerManager.viewer.navigation.setCameraUpVector(
-              new THREE.Vector3(-0.4082483040149399,
+              new THREE.Vector3( -0.4082483040149399,
                 0.4082483175660186,
-                0.8164965606011091)
+                0.8164965606011091 )
             );
             this.viewerManager.viewer.navigation.setView(
-              new THREE.Vector3(3266.613776624011,-3705.1753901778,3340.975522291141),
-              new THREE.Vector3(-170.97430537761724, -267.58719407153,
-                -96.61287349825356));
+              new THREE.Vector3( 3266.613776624011, -3705.1753901778, 3340.975522291141 ),
+              new THREE.Vector3( -170.97430537761724, -267.58719407153,
+                -96.61287349825356 ) );
+
+            this.viewerManager.viewer.removeEventListener( Autodesk.Viewing.GEOMETRY_LOADED_EVENT, this.onGeometryLoadedBinded );
+            this.adjustLogos( {
+              width: this.viewerManager.viewer.container.clientWidth
+            } );
+            this.viewerManager.viewer.addEventListener( Autodesk.Viewing.VIEWER_RESIZE_EVENT, this.adjustLogobinded );
             this.initialized = true;
 
           } );
@@ -160,12 +185,12 @@
         }
         let toDisplay = [];
         for (let i = 0; i < this.lastDbIds.length; i++) {
-          if (res.indexOf(this.lastDbIds[i]) === -1){
-            toDisplay.push(this.lastDbIds[i])
+          if (res.indexOf( this.lastDbIds[i] ) === -1) {
+            toDisplay.push( this.lastDbIds[i] )
           }
         }
 
-        this.viewerManager.show(toDisplay);
+        this.viewerManager.show( toDisplay );
         this.lastDbIds = res;
         this.viewerManager.hide( res, this.model );
       },
@@ -203,11 +228,29 @@
             }
             return null;
           } );
+      },
+      adjustLogos( e ) {
+        this.ratio = this.oldWidth !== 1 ?   e.width / this.oldWidth  : 1;
+        this.oldWidth = e.width;
+        this.displayLogo = this.viewerManager.viewer.container.clientWidth > 640;
+        const colas = document.getElementById( "logo-colas" );
+        const spinal = document.getElementById( "logo-spinalcom" );
+        this.adjustLogo( colas );
+        this.adjustLogo( spinal );
+
+      },
+      adjustLogo( elt ) {
+        if (elt !== null) {
+          elt.style.width = parseInt( elt.style.width ) * this.ratio + "px";
+          elt.style.height = parseInt( elt.style.height ) * this.ratio + "px"
+        }
       }
     },
     mounted() {
-      window.$cookies.config( '90d' );
-      if(!this.$store.state.isConnected){
+
+      this.adjustLogobinded = this.adjustLogos.bind( this );
+      this.onGeometryLoadedBinded = this.onGeometryLoaded.bind( this );
+      if (!this.$store.state.isConnected) {
         const cookie = window.$cookies.get( 'user' );
         if (typeof cookie === 'undefined' || cookie === '' || cookie === null) this.$router.push( 'login' );
         else {
@@ -218,12 +261,20 @@
             } );
         }
       }
+
     },
+
+    beforeDestroy() {
+      this.viewerManager.viewer.removeEventListener( Autodesk.Viewing.VIEWER_RESIZE_EVENT, this.adjustLogobinded )
+    }
   };
 </script>
 
 <style scoped>
+    .home {
+        max-height: -webkit-fill-available;
 
+    }
 
     #logo-colas {
         position: absolute;
@@ -239,10 +290,11 @@
         z-index: 1;
     }
 
-    #viewer {
+    .viewer {
         height: 100vh;
         position: relative;
     }
+
 
     .header {
         position: absolute;
