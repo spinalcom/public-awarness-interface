a8adb8cc61ae945a175e788b45d2149efb30e119
LorisC <loris@ciprin.fr>
2019-10-01 11:37:19 +0200

    adjust window this for smartphone

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -247,6 +247,10 @@
       }
     },
     mounted() {
+      // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+      let vh = window.innerHeight * 0.01;
+      // Then we set the value in the --vh custom property to the root of the document
+      document.documentElement.style.setProperty('--vh', `${vh}px`);
 
       this.adjustLogobinded = this.adjustLogos.bind( this );
       this.onGeometryLoadedBinded = this.onGeometryLoaded.bind( this );
@@ -278,8 +282,8 @@
 
 <style scoped>
     .home {
-        max-height: -webkit-fill-available;
-
+        height: 100vh; /* Fallback for browsers that do not support Custom Properties */
+        height: calc(var(--vh, 1vh) * 100);
     }
 
     #logo-colas {
